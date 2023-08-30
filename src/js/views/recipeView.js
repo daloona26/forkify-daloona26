687fc339c0ceeae67ead572366a27601863d22a2
ahmad <daloona26@gmail.com>
2023-08-30 13:20:59 +0300

    Added new welcome feature

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -1,6 +1,7 @@
 import icons from 'url:../../img/icons.svg';
-import { Fraction } from 'fractional';
 import View from './View.js';
+import fracty from 'fracty';
+
 class RecipeView extends View {
   _parentElement = document.querySelector('.recipe');
   _errorMessage = `We could not find that recipe. Please try another one! `;
@@ -123,7 +124,7 @@ class RecipeView extends View {
       <use href="src/img/icons.svg#icon-check"></use>
       </svg>
       <div class="recipe__quantity">${
-        ing.quantity ? new Fraction(ing.quantity).toString() : ''
+        ing.quantity ? fracty(ing.quantity).toString() : ''
       }</div>
       <div class="recipe__description">
       <span class="recipe__unit">${ing.unit}</span>
